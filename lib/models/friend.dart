e4584210987558ec2d6a01a7dbc590f96e3a26da
nhulston <nicholashulston@gmail.com>
2022-08-09 22:02:22 -0700

    Starter Project

--- lib/models/friend.dart
+++ lib/models/friend.dart
@@ -1,8 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-import '../style.dart';
-
 class Friend {
   Friend(this.image, this.name, this.status, this.time, this.streak);
 
